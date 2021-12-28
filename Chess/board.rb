5523869fb3073e584abf79d4f50f4ee5d67338d0
sean <midruss@yahoo.com>
2021-12-28 11:57:58 -0500

    reasoned through board and piece logic

--- Chess/board.rb
+++ Chess/board.rb
@@ -1,18 +1,25 @@
 require_relative "piece"
 class Board
+
     def initialize
         @grid = Array.new(8) {Array.new(8) {NullPiece.instance}} 
+        #private
+        #@null_piece #fill board with null piece
     end
     
     def [](pos)
-
+        return @grid[pos[0]][pos[1]]
     end
 
     def []=(pos,val)
-
+        @grid[pos[0]][pos[1]] = val
     end
 
     def move_piece(start_pos, end_pos)
 
     end
+
+    #def fill_board
+    #    self[[row, col]] = #pieces
+    #end
 end
