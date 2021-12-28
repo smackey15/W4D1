4b864536daebfed94651bda14ec30881d144b875
Deanding <dingtian.ding@gmail.com>
2021-12-28 13:35:29 -0500

    setup the grid

--- Chess/board.rb
+++ Chess/board.rb
@@ -1,8 +1,9 @@
 require_relative "piece"
 class Board
+    attr_reader :grid
 
     def initialize
-        @grid = Array.new(8) {Array.new(8) {NullPiece.instance}} 
+        @grid = Array.new(8) {Array.new(8)} #{NullPiece.instance}
         #private
         #@null_piece #fill board with null piece
     end
@@ -15,7 +16,11 @@ class Board
         @grid[pos[0]][pos[1]] = val
     end
 
-    def move_piece(start_pos, end_pos)
+    def move_piece(start_pos, end_pos) 
+        #raise error if no piece at start_pos
+        #raise error if piece can't move to end_pos 
+        #should update 2d grid
+        #should update the moved pieces position
 
     end
 
